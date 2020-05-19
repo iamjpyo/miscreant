00622792318e9d09269a8833b80a49de2ee17b5a
Juan Pablo <jpluncumilla@gmail.com>
2020-05-19 03:00:44 -0400

    Miscreant v1.0

---
+++ assets/app.js
@@ -0,0 +1,47 @@
+$("body").ready(function(){
+ 
+    $(".slide").hide();
+    $(".show").show();
+
+$('.menu-list').click(function(){
+$(".slide").slideToggle();
+});
+
+});
+
+$("body").ready(function(){
+ 
+    $(".slide2").hide();
+    $(".show").show();
+
+$('.menu-list2').click(function(){
+$(".slide2").slideToggle();
+});
+
+});
+
+$("body").ready(function(){
+ 
+    $(".slide3").hide();
+    $(".show").show();
+
+$('.menu-list3').click(function(){
+$(".slide3").slideToggle();
+});
+
+});
+
+$("body").ready(function(){
+ 
+    $(".slide4").hide();
+    $(".show").show();
+
+$('.menu-list4').click(function(){
+$(".slide4").slideToggle();
+});
+
+});
+
+//$(".menu-list").click(function() {
+ //   $(".slide").slideToggle( "slow" );
+  //});
